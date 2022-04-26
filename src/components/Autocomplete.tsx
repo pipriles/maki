a34a6136992f827ceac459f3fa1c4d56b56de8f5
Oswaldo Capriles <oswald.capriles46@gmail.com>
2022-04-26 19:56:35 -0300

    Changed Autocomplete component

--- src/components/Autocomplete.tsx
+++ src/components/Autocomplete.tsx
@@ -3,21 +3,29 @@ import { createAppUseStyles } from '../styles';
 
 const useStyles = createAppUseStyles(theme => ({
   root: {
-    position: "relative",
     width: "100%",
+    position: "relative",
   },
   options: {
-    position: "fixed",
+    position: "absolute",
     backgroundColor: theme.lighten(theme.palette.background, 0.15),
     listStyle: "none",
     margin: 0,
     padding: 0,
-    height: 200,
+    maxHeight: 200,
     overflow: "auto",
+    zIndex: 99,
+    bottom: "100%",
+    width: "100%",
+    // transform: "translateY(-100%)",
   },
   option: {
     fontSize: 12,
     padding: theme.spacing(1),
+    cursor: "pointer",
+    '&:hover, &:focus': {
+      backgroundColor: theme.lighten(theme.palette.background, 0.5),
+    }
   }
 }));
 
@@ -30,27 +38,18 @@ interface AutocompleteProps {
 
 const Autocomplete = ({ value, options, onChange, className }: AutocompleteProps) => {
 
-  const element = React.useRef<HTMLInputElement>(null);
   const styles = useStyles();
 
   const suggestions = options.map(opt => (
     <li key={opt} className={styles.option}>{opt}</li>
   ));
 
-  const rect = element.current?.getBoundingClientRect();
-  const boxStyles = { top: rect?.top ?? 'unset', transform: "translateY(-100%)" }
-
   return (
     <div className={styles.root}>
-      <input 
-          ref={element}
-          className={className} 
-          value={value}
-          onChange={onChange}
-        />
-        <ul className={styles.options} style={boxStyles}>
-          {suggestions}
-        </ul>
+      <input className={className} value={value} onChange={onChange} />
+      <ul className={styles.options}>
+        {suggestions}
+      </ul>
     </div>
   );
 };
